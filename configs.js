392c86b24cb36fde5c0ef8268290beff36b7e64a
katty <katty944@gmail.com>
2020-05-06 10:24:51 +0200

    cambios
    cambios

---
+++ configs.js
@@ -0,0 +1,13 @@
+properties = {
+	userLocationRefresh : 2000, // milliseconds
+
+	shortDistance 		: 100,   // meters
+	mediumDistance 		: 200,   // meters
+	longDistance 		: 300,    // meters
+
+	kmlFiles 			: [
+		'1.kml', 
+		'2.kml', 
+		'3.kml'
+	]
+}
