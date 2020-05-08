7b74ad716ebe3c1bd627cfc0ca077cff38e79a98
katty <katty944@gmail.com>
2020-05-08 10:29:16 +0200

    aa
    aa

---
+++ configs.js
@@ -0,0 +1,15 @@
+properties = {
+	userLocationRefresh : 2000, // milliseconds
+
+	notificationTimeout : 10000, // milliseconds
+
+	shortDistance 		: 100,   // meters
+	mediumDistance 		: 200,   // meters
+	longDistance 		: 1000,    // meters
+
+	kmlFiles 			: [
+		'1.kml', 
+		'2.kml', 
+		'3.kml'
+	]
+}
